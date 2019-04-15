f69edb515e11f8340538c9e22f38d94535bd023f
Kapa <tylahkapa@gmail.com>
2019-04-15 22:02:08 +1000

    Spawning a new card and claiming
    Pipes can now spawn a new card on command and anyone in that channel can "claim that card".

---
+++ embeds.js
@@ -0,0 +1,120 @@
+//Generic Embed, Presents Card
+function Card(c) {
+  var embed = {
+    embed: {
+      title: c.name,
+      url: c.scryfall_uri,
+      color: 111111,
+      timestamp: "2019-04-14T10:39:38.772Z",
+      image: {
+        url: c.image_uris.border_crop
+      },
+      author: {
+        name: "New Card"
+      },
+      fields: [
+        {
+          name: "Description",
+          value: c.oracle_text,
+          inline: true
+        },
+        {
+          name: "Set",
+          value: c.set_name,
+          inline: true
+        },
+        {
+          name: "Artist",
+          value: c.artist,
+          inline: true
+        },
+        {
+          name: "Image not showing?",
+          value: `Card image available [here](${c.image_uris.border_crop})`
+        }
+      ]
+    }
+  };
+
+  return embed;
+}
+
+//Embed that shows only art, set and artist
+function NameGuess(c) {
+  var embed = {
+    embed: {
+      url: c.scryfall_uri,
+      color: 111111,
+      timestamp: "2019-04-14T10:39:38.772Z",
+      image: {
+        url: c.image_uris.art_crop
+      },
+      author: {
+        name: "Name the Card!"
+      },
+      fields: [
+        {
+          name: "Set",
+          value: c.set_name,
+          inline: true
+        },
+        {
+          name: "Artist",
+          value: c.artist,
+          inline: true
+        },
+        {
+          name: "Image not showing?",
+          value: `Card image available [here](${c.image_uris.border_crop})`
+        }
+      ]
+    }
+  };
+
+  return embed;
+}
+
+function ClaimedCard(c) {
+  var embed = {
+    embed: {
+      title: c.name,
+      url: c.scryfall_uri,
+      color: 111111,
+      timestamp: "2019-04-14T10:39:38.772Z",
+      image: {
+        url: c.image_uris.border_crop
+      },
+      author: {
+        name: "Card Claimed!"
+      },
+      fields: [
+        {
+          name: "Description",
+          value: c.oracle_text,
+          inline: true
+        },
+        {
+          name: "Set",
+          value: c.set_name,
+          inline: true
+        },
+        {
+          name: "Artist",
+          value: c.artist,
+          inline: true
+        },
+        {
+          name: "Image not showing?",
+          value: `Card image available [here](${c.image_uris.border_crop})`
+        }
+      ]
+    }
+  };
+
+  return embed;
+}
+module.exports = {
+  Card,
+  NameGuess,
+  ClaimedCard
+};
--- scryfallreq.js
+++ scryfallreq.js
@@ -2,9 +2,16 @@ const fetch = require('node-fetch');
 
 //This script deals with contacting the Scryfall API
 
-//Draws aa random card
+//Draws a random card
 async function RandomCard(){
   var c = fetch('https://api.scryfall.com/cards/random')
+    .then(handle);
+
+  return await c;
+}
+
+async function FetchCard(uri){
+  var c = fetch(uri)
     .then(handle);
 
   return await c;
@@ -16,83 +23,7 @@ function handle(res){
   });
 }
 
-//Handles Embed content for discord
-function Card(c) {
-  var embed = {
-      "embed": {
-        "title": c.name,
-        "url": c.scryfall_uri,
-        "color": 111111,
-        "timestamp": "2019-04-14T10:39:38.772Z",
-        "image": {
-          "url": c.image_uris.border_crop
-        },
-        "author": {
-          "name": "New Card"
-        },
-        "fields": [
-          {
-            "name": "Description",
-            "value": c.oracle_text,
-            "inline": true
-          },
-          {
-            "name": "Set",
-            "value": c.set_name,
-            "inline": true
-          },
-          {
-            "name": "Artist",
-            "value": c.artist,
-            "inline": true
-          },
-          {
-            "name": 'Image not showing?',
-            "value": `Card image available [here](${c.image_uris.border_crop})`
-          }
-        ]
-      }
-  };
-
-  return embed;
-}
-
-function NameGuess(c){
-  var embed = {
-    "embed": {
-      "url": c.scryfall_uri,
-      "color": 111111,
-      "timestamp": "2019-04-14T10:39:38.772Z",
-      "image": {
-        "url": c.image_uris.art_crop
-      },
-      "author": {
-        "name": "Name the Card!"
-      },
-      "fields": [
-        {
-          "name": "Set",
-          "value": c.set_name,
-          "inline": true
-        },
-        {
-          "name": "Artist",
-          "value": c.artist,
-          "inline": true
-        },
-        {
-          "name": 'Image not showing?',
-          "value": `Card image available [here](${c.image_uris.border_crop})`
-        }
-      ]
-    }
-  };
-
-  return embed;
-}
-
 module.exports = {
   RandomCard,
-  Card,
-  NameGuess
+  FetchCard
 };
