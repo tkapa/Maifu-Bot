ae0b478b99012de8b2e4ed9134031287e8708dbb
Kapa <tylahkapa@gmail.com>
2019-04-18 00:50:39 +1000

    Changed bot to be a command bot, much better
    Pipes is now a command bot, which makes defining what commands should do what much easier. There's now some interaction between pipes and the database but cannot read and write properties to it yet.

--- database.js
+++ database.js
@@ -8,12 +8,22 @@ const client = new Client({
   database: "testing_database"
 });
 
+//Establishes a connection to the database
 function EstablishConnection(){
   client.connect()
-  .then(()=> console.log('Connected Successfully!'))
-  .catch(e=>console.log(e));
+    .then(()=> console.log('Connected Successfully!'))
+    .catch(e=>console.log(e));
 }
 
+//Testing Connectiong Functionality First
+function Testing(){
+  client.query("SELECT * from maifu_users_2019_4_17")
+    .then(results => console.table(results.rows))
+    .catch(e=>console.log(e));
+}
+//maifu_users_2019_4_17
+
 module.exports = {
-  EstablishConnection
+  EstablishConnection,
+  Testing
 };
--- index.js
+++ index.js
@@ -1,35 +1,96 @@
 const auth = require("./auth.json");
 const db = require("./database.js");
+const embd = require("./embeds.js");
+const mtg = require("./scryfall.js");
 const Eris = require("eris");
-const bot = new Eris(auth[0].token);
+const bot = new Eris.CommandClient(auth[0].token, {}, {
+  description: "Budget Card Collecting",
+  owner: "Kapa",
+  prefix: "m."
+});
 
 var cards = [];
 
 //Logs what happens when a bot connects to Discord
 bot.on("ready", () => {
-  console.log("Ready!");
   db.EstablishConnection();
 });
 
-//When a new message is sent in a guild, check it for commands
-bot.on("messageCreate", msg => {
-  var m = msg.content.toLowerCase();
-  if (m === "m") {
-    RandomCard(msg);
+//
+bot.registerCommand("rand", (msg, args) =>{
+  mtg
+    .RandomCard()
+    .then(c => bot.createMessage(msg.channel.id, embd.Card(c)))
+    .catch(e => console.log(e));
+  },
+  {
+    description: "A random card from the void",
+    fullDescription: "A random card from the void"
   }
-  if (m === "s") {
-    SpawnCard(msg);
+);
+
+//Command forces a card to spawn
+bot.registerCommand("spawn", (msg, args)=>{
+  mtg
+    .RandomCard()
+    .then(c => {
+      bot.createMessage(msg.channel.id, embd.NameGuess(c));
+      StoreCard(msg.channel.id, c.name, c.uri);
+      console.log(c.name);
+    })
+    .catch(e => console.log(e));
+  },
+  {
+    description: "This is a secret command",
+    requirements: {
+      userIDs: ["142548196089004032"]
+    }
   }
-  if (m.split(" ", 1)[0] === "c") {
-    ClaimCard(msg.channel.id, m.slice(2));
+);
+
+//
+bot.registerCommand("claim", (msg, args)=>{
+  var id = msg.channel.id;
+  
+  
+  console.log(args);
+  for (var i = 0; i <= cards.length - 1; i++) {
+    if (cards[i][0] === id) {
+      if (cards[i][1].toLowerCase() === args.join(" ").toLowerCase()) {
+        //Confirm Claim
+        mtg
+          .FetchCard(cards[i][2])
+          .then(c => bot.createMessage(id, embd.ClaimedCard(c)));
+        
+        cards.splice(i, 1);
+      } else {
+        //Wrong card name
+        bot.createMessage(id, "Card Name is Incorrect! Try again.")
+      }
+    } else {
+      //No cards on the channel
+      bot.createMessage(id, "No cards to be claimed on this channel.");
+    }
   }
-});
+  },
+  {
+    description: "This is a secret command",
+  }
+);
+
+//Stores a card in a local 2D array to be collected later
+//Replaces any unclaimed cards
+function StoreCard(id, name, uri) {
+  
+  if (cards.length > 0){
+    for(var i = 0; i >=cards.length-1; i++) {
+      if(cards[i][0] === id){
+        cards.splice(i,1);
+      }
+    }
+  }
+  var newCard = [id, name, uri];
+  cards.push(newCard);
+}
+
 bot.connect();
-
-function Send(id, content){
-  bot.createMessage(id, content);
-}
-
-module.exports = {
-  Send
-}
